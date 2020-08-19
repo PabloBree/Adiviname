cc33993cccda91da07ab92ae4ef16f22df13e350
gonzavai <gonzavai_96@hotmail.com>
2020-08-19 16:15:57 -0300

    Version Gonzalo

--- app/src/main/java/com/lugopa/juegoelectiva/JugarActivity.java
+++ app/src/main/java/com/lugopa/juegoelectiva/JugarActivity.java
@@ -2,7 +2,10 @@ package com.lugopa.juegoelectiva;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class JugarActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class JugarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jugar);
+
+        // CODIGO PARA REPRODUCIR VIDEO EN LA APP - No funciona
+        VideoView videoView = findViewById(R.id.videoView_intro);
+        String videoPath = "android.resource://com.lugopa.juegoelectiva/"+ R.raw.video_intro;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        videoView.start();
+//        MediaController mediaController = new MediaController(this);
+//        videoView.setMediaController(mediaController);
+//        mediaController.setAnchorView(videoView);
     }
+
+
 }
--- app/src/main/java/com/lugopa/juegoelectiva/MainActivity.java
+++ app/src/main/java/com/lugopa/juegoelectiva/MainActivity.java
@@ -2,6 +2,7 @@ package com.lugopa.juegoelectiva;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     // inicializar elementos UI
-    private Button btn_iniciar;
+    private Button btn_jugar;
     private Button btn_instrucciones;
     private Button btn_salir;
     private Button btn_configuracion;
@@ -21,11 +22,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        inicializar();
+        inicializar(); // obtengo las vistas de UI
 
-        btn_iniciar.setOnClickListener(new View.OnClickListener() {
+        btn_jugar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                abrir_jugar();
+            }
+        });
+
+        btn_puntajes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrir_puntajes();
+            }
+        });
+
+        btn_instrucciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrir_instrucciones();
+            }
+        });
+
+        btn_configuracion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrir_configuracion();
+            }
+        });
+
+        btn_salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //salir();
             }
         });
 
@@ -38,15 +68,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void inicializar(){
-        btn_iniciar = findViewById(R.id.button_iniciar);
+        btn_jugar = findViewById(R.id.button_iniciar);
         btn_configuracion = findViewById(R.id.button_configuracion);
         btn_instrucciones = findViewById(R.id.button_instrucciones);
         btn_puntajes = findViewById(R.id.button_puntajes);
         btn_salir = findViewById(R.id.button_salir);
     }
 
-    private void iniciarJuego(){
+    private void abrir_jugar(){
         // abre activity jugar
+        Intent intent = new Intent(this, JugarActivity.class);
+        //intent.putExtra( lo que queramos pasar) // para pasar cosas a la activity
+        startActivity(intent);
+    }
+
+    private void abrir_instrucciones(){
+        // abre activity instrucciones
+        Intent intent = new Intent(this, InstruccionesActivity.class);
+        //intent.putExtra( lo que queramos pasar) // para pasar cosas a la activity
+        startActivity(intent);
+    }
+
+    private void abrir_configuracion(){
+        // abre activity configuracion
+        Intent intent = new Intent(this, ConfiguracionActivity.class);
+        //intent.putExtra( lo que queramos pasar) // para pasar cosas a la activity
+        startActivity(intent);
+    }
+
+    private void abrir_puntajes(){
+        // abre activity puntajes
+        Intent intent = new Intent(this, PuntajesActivity.class);
+        //intent.putExtra( lo que queramos pasar) // para pasar cosas a la activity
+        startActivity(intent);
+    }
+
+    private void salir(){
+        // DEBERIA CERRAR LA APP o la sesion
     }
     
 
